cf5f26a12e2c8287769932a6b262f21d20cddb3a
Erick Rangili <erickrangili@gmail.com>
2019-12-23 18:56:40 +0300

    prompt to pay working correctly

---
+++ keys.py
@@ -0,0 +1,5 @@
+business_shortcode = "174379" #Lipa na MPESA Short code
+phone_number = "254703882375"
+lipa_na_mpesa_passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+consumer_key = "cZGG0fUiorISXJV60G2eA5MWu99CAWFZ"
+consumer_secret = "xvAX0dE5PPB8S6lS"
--- lipanampesa.py
+++ lipanampesa.py
@@ -1,1 +1,55 @@
-print("Tutalipa na MPESA")
+import base64
+import requests
+from requests.auth import HTTPBasicAuth
+import keys
+from datetime import datetime
+
+
+unformatted_time = datetime.now()
+formatted_time = unformatted_time.strftime("%Y%m%d%H%M%S")
+
+data_to_encode = keys.business_shortcode + keys.lipa_na_mpesa_passkey + formatted_time
+data_to_encode = data_to_encode.encode('utf-8') #changing string to bytes
+encoded_string = base64.b64encode(data_to_encode)
+# decoded_string = base64.b64decode(encoded_string)
+decoded_string = encoded_string.decode('utf-8')
+Password = decoded_string
+# print(Password)
+
+
+  
+  
+consumer_key = keys.consumer_key
+consumer_secret = keys.consumer_secret
+api_URL = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+  
+r = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
+  
+json_response = r.json() #{'access_token': '9ifzdu3t3yuAm2nwB23JRxnxe3Kv', 'expires_in': '3599'}
+
+my_access_token = json_response['access_token']
+
+
+def lipana_mpesa():
+    access_token = my_access_token
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+    headers = { "Authorization": "Bearer %s" % access_token }
+    request = {
+    "BusinessShortCode": keys.business_shortcode,
+    "Password": Password,
+    "Timestamp": formatted_time,
+    "TransactionType": "CustomerPayBillOnline",
+    "Amount": "5",
+    "PartyA": keys.phone_number,
+    "PartyB": keys.business_shortcode,
+    "PhoneNumber": keys.phone_number,
+    "CallBackURL": "https://agottech.com",
+    "AccountReference": "1245",
+    "TransactionDesc": "Download"
+    }
+    
+    response = requests.post(api_url, json = request, headers=headers)
+    
+    print (response.text)
+
+lipana_mpesa()
